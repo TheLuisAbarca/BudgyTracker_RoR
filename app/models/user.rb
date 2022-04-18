05e95a189eaff5b2817f7fdffcbf2805b94c06e6
TheLuisAbarca <arclaav.adm@gmail.com>
2022-04-18 17:51:39 -0500

    Add Entities table, controller and routes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :entities
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
