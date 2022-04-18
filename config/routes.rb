9ec5e7cfd93c33db60f45c083dbb4d94ce9e42c7
TheLuisAbarca <arclaav.adm@gmail.com>
2022-04-18 14:41:31 -0500

    Add resources for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
   root to: 'home#index'
+  resources :users, only: [:index]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
